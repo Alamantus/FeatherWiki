f37e250335aefd351e873059aacbcc4d0de8cbb0
Robbie Antenesse <dev@alamantus.com>
2022-04-14 22:55:29 -0600

    Super basic internal wiki link parsing

--- views/page/display.js
+++ views/page/display.js
@@ -2,13 +2,22 @@ import html from 'choo/html';
 import raw from 'choo/html/raw';
 
 export const pageDisplay = (state, emit, page) => {
+  const { content } = page;
+  let c = content ?? null;
+  if (c) {
+    (content ? content.match(/(\[\[.+\]\])?/g) : []).map(l => {
+      const name = l.replace('[[', '').replace(']]', '');
+      return {
+        match: l,
+        link: `<a href="${state.siteRoot}?page=${state.help.slugify(name)}">${name}</a>`,
+      };
+    }).forEach(l => {
+      c = c.replace(l.match, l.link);
+    });
+  }
   return [
     html`<article>
-      ${
-        page?.content
-        ? raw(page.content)
-        : 'No Page Content'
-      }
+      ${ c ? raw(c) : 'No Page Content' }
     </article>`,
     html`<footer class=r>
       <div class="c w34">
