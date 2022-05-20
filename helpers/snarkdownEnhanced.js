e99922374b2dc7d9fa9c47a9fa30412d6f724569
Robbie Antenesse <dev@alamantus.com>
2022-05-20 11:47:07 -0600

    Make snarkdown not format within HTML or wiki links

--- helpers/snarkdownEnhanced.js
+++ helpers/snarkdownEnhanced.js
@@ -1,12 +1,26 @@
 import md from 'snarkdown';
 
 export default function snarkdownEnhanced(markdown) {
-  return markdown
-    .split(/(?:\r?\n){2,}/)
+  const a = '\uFFFF', u = '\uFFFE', l = '\uFFFD', r = '\uFFFC';
+  // Find any HTML tags with underscore or asterisk or internal wiki links
+  (markdown.match(/<.*?[_*].*?(?=>)|\[\[.+?\]\]/gs) ?? []).forEach(m => {
+    markdown = markdown.replace(
+      m,
+      m.replace(/_/g, u)
+        .replace(/\*/g, a)
+        .replace(/\[\[/g, l)
+        .replace(/\]\]/g, r)
+    );
+  });
+  return markdown.split(/(?:\r?\n){2,}/)
     .map(l =>
       [" ", "\t", "#", "-", "*", ">"].some(c => l.startsWith(c))
         ? md(l)
         : `<p>${md(l)}</p>`
     )
-    .join("\n");
+    .join("\n")
+    .replace(new RegExp(u, 'g'), '_')
+    .replace(new RegExp(a, 'g'), '*')
+    .replace(new RegExp(l, 'g'), '[[')
+    .replace(new RegExp(r, 'g'), ']]');
 }
