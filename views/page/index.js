5ce6b0d1efc9e8422d4e097c3f74407eb3a371fc
Robbie Antenesse <dev@alamantus.com>
2022-05-20 12:54:31 -0600

    Prevent crash on 404

--- views/page/index.js
+++ views/page/index.js
@@ -20,21 +20,25 @@ export const pageView = (state, emit, page) => {
       ${breadcrumb.map(p => [html`<a href="${state.siteRoot}?page=${p.slug}">${p.name}</a>`, ' / '])}
       <div class="r ns">
         <h1 class=c>${page.name}</h1>
-        <div class="c w14 tr">
-          <time datetime=${modified.toISOString()}>
-            ${
-              crFormat !== mdFormat
-              ? html`<abbr title="Created: ${crFormat}">${mdFormat}</abbr>`
-              : mdFormat
-            }
-          </time>
         ${
-          !p.published
-          ? html`<button onclick=${() => emit(events.START_EDIT)}>Edit</button>
-          `
-          : ''
+          page.e
+          ? ''
+          : html`<div class="c w14 tr">
+            <time datetime=${modified.toISOString()}>
+              ${
+                crFormat !== mdFormat
+                ? html`<abbr title="Created: ${crFormat}">${mdFormat}</abbr>`
+                : mdFormat
+              }
+            </time>
+          ${
+            !p.published
+            ? html`<button onclick=${() => emit(events.START_EDIT)}>Edit</button>
+            `
+            : ''
+          }
+          </div>`
         }
-        </div>
       </div>
     </header>`,
     pageDisplay(state, emit, page)
