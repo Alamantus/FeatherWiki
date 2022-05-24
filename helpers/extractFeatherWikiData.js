138d66f91d58650da52cdcc80581276ab107e8dd
Robbie Antenesse <dev@alamantus.com>
2022-05-24 17:55:46 -0600

    Reduce repetition in extractFeatherWikiData

--- helpers/extractFeatherWikiData.js
+++ helpers/extractFeatherWikiData.js
@@ -1,30 +1,33 @@
 import { decompress } from './jsonCompress';
 
-export function extractFeatherWikiData(file, callback = () => {}) {
+export function extractFeatherWikiData (file, callback = () => {}) {
   const reader = new FileReader();
-  reader.onload = async e => {
-    // The '\\s' in each regex start string makes sure it doesn't match *that* line.
-    // Feather Wiki's HTML output has spaces between elements, which is important in this case because it helps us match the content.
-    const p = '\\s<script id=p type=application/json>'; // Start of wiki data regular expression
-    const pm = e.target.result.match(new RegExp(p + '.+?(?=</script>)', 'gs'));
-    if ((pm ?? []).length < 1) return alert('Could not find {{package.json:title}} data.');
-    const pd = decompress(JSON.parse(pm[0].replace(p.replace('\\s', ''), '').trim()));
-    const c = '\\s<style id=c>'; // Start of custom CSS regular expression.
-    const cm = e.target.result.match(new RegExp(c + '.+?(?=</style>)', 'gs'));
-    let cd = '';
-    if ((cm ?? []).length) cd = cm[0].replace(c.replace('\\s', ''), '').trim();
-    const j = '\\s<script id=j>'; // Start of custom JS regular expression.
-    const jm = e.target.result.match(new RegExp(j + '.+?(?=</script>)', 'gs'));
-    let jd = '';
-    if ((jm ?? []).length) jd = jm[0].replace(j.replace('\\s', ''), '').trim();
-    pd.img = await migrateImg(pd.img);
-    callback([pd, cd, jd]);
+  reader.onload = async event => {
+    const file = event.target.result;
+    const pString = matchHtml(file, 'script', 'p type=application/json');
+    // validate that wiki data exists and is probably JSON
+    if (!pString.length || pString[0] !== '{') return alert('Could not find {{package.json:title}} data.');
+    const pData = decompress(JSON.parse(pString));
+    pData.img = await migrateImg(pData.img);
+    callback([
+      pData,
+      matchHtml(file, 'style', 'c'),
+      matchHtml(file, 'script', 'j'),
+    ]);
   };
   reader.onerror = function (e) {
     if (process.env.NODE_ENV !== 'production') console.error(e);
     callback(null);
   };
   reader.readAsText(file);
+}
+
+function matchHtml (source, element, id) {
+  // The '\\s' in each regex start string makes sure it doesn't match *that* line but does match the HTML with a space before it.
+  // Feather Wiki's HTML output has spaces between elements, which is important in this case because it helps us match the content.
+  const start = `\\s<${element} id=${id}>`; // Start of custom CSS regular expression.
+  const match = source.match(new RegExp(start + `.+?(?=</${element}>)`, 's'));
+  return ((match ?? []).length) ? match[0].replace(start.replace('\\s', ''), '').trim() : '';
 }
 
 async function migrateImg (img) {
