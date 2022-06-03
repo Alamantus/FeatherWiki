3c0f33bf59a0b9d6a0e8423e976b9c38a6275eb0
Robbie Antenesse <dev@alamantus.com>
2022-06-03 16:05:32 -0600

    Imperfect md code block parse (#37); parse md hr tag

--- helpers/snarkdownEnhanced.js
+++ helpers/snarkdownEnhanced.js
@@ -12,12 +12,26 @@ export default function snarkdownEnhanced(markdown) {
         .replace(/\]\]/g, r)
     );
   });
+  let c = false, b = '```'; // code block
+  const s = h => h.replace(/</g, '&lt;').replace(/>/g, '&gt;'); // oversimplified HTML sanitizer
   return markdown.split(/(?:\r?\n){2,}/)
-    .map(l =>
-      [" ", "\t", "#", "-", "*", ">"].some(c => l.startsWith(c))
+    .map(l => {
+      const lt = l.trim();
+      if (l.includes(b)) {
+        // Parse paragraph as a code block if it contains ```
+        const o = s(l) // Sanitize any HTML tags
+          .replace(b, `<${c ? '/' : ''}pre>`) // Change first instance of ``` to a pre tag. If we're in a code block, assume it's the end
+          .replace(b, '</pre>'); // If any ``` remain, assume it's the end.
+        c = !lt.endsWith(b) || (!c && lt === b);
+        return o;
+      }
+      if (!c && lt.match(/^[-*]{2,}$/)) return '<hr>';
+      return c ? s(l) : ( // If we're in a code block, don't transform the text until we're out of it again.
+        [" ", "\t", "#", "-", "*", ">"].some(c => l.startsWith(c))
         ? md(l)
         : `<p>${md(l)}</p>`
-    )
+      );
+    })
     .join("\n")
     .replace(new RegExp(u, 'g'), '_')
     .replace(new RegExp(a, 'g'), '*')
