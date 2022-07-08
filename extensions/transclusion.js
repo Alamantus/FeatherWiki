fe94b3f68fa8a2df5aa1befe61f4d6bbe58a1053
Robbie Antenesse <dev@alamantus.com>
2022-07-07 20:56:14 -0600

    Improve transclusion extension

--- extensions/transclusion.js
+++ extensions/transclusion.js
@@ -6,8 +6,11 @@ else (({state, emitter}) => transclusionExtension(state, emitter))(window.FW);
 
 function transclusionExtension (state, emitter) {
   const { events } = state;
+  state.tDepth = 0;
+  state.tDepthMax = 20; // Maximum depth to check for transclusion
   [events.DOMCONTENTLOADED, events.RENDER].forEach(ev => {
     emitter.on(ev, () => {
+      state.tDepth = 0;
       setTimeout(() => { // Adds a very small delay so it injects after render when elements exist in DOM
         injectTransclusion();
       }, 300);
@@ -18,7 +21,8 @@ function transclusionExtension (state, emitter) {
   function injectTransclusion() {
     if (state.pg) {
       const uc = document.querySelector('.uc');
-      (uc?.innerHTML?.match(/{{.+?(?=}})/g) ?? []).forEach(l => {
+      const matches = uc?.innerHTML?.match(/{{.+?(?=}})/g) ?? [];
+      matches.forEach(l => {
         const slug = l.replace('{{', '').trim();
         const page = state.p.pages.find(pg => pg.slug === slug);
         console.log(slug, page);
@@ -27,7 +31,7 @@ function transclusionExtension (state, emitter) {
         const pageContent = img(
           pg(
             hLink(
-              out(`<h1 id=${page.slug}>${page.name} <a href="?page=${page.slug}" class="fr h">Go to Page</a></h1>${page.content}`)
+              out(`<h1 id=${page.slug}>${page.name} <a internal href="?page=${page.slug}" class="fr h">Go to Page</a></h1>${page.content}`)
             ),
             state
           ),
@@ -38,6 +42,12 @@ function transclusionExtension (state, emitter) {
           `<article class="transclusion">${pageContent}</article>`
         );
       });
+      if (matches.length > 0 && state.tDepth < state.tDepthMax) {
+        state.tDepth++;
+        // If transclusion is injected, run again to see if more is needed
+        // Only run it state.tDepthMax times to prevent potential infinite recursion
+        injectTransclusion();
+      }
     }
   }
 }
