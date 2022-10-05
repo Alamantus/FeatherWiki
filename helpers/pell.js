155596fbe814cdd62b51b9e6836cf8f4b4c89970
Robbie Antenesse <dev@alamantus.com>
2022-10-05 15:03:00 -0600

    Move FW pell customizations into pell code to save bytes!

--- helpers/pell.js
+++ helpers/pell.js
@@ -2,145 +2,141 @@
  * Modified from pell, the simplest and smallest WYSIWYG text editor for web, with no dependencies
  * https://github.com/jaredreich/pell
  * 
+ * Several customization options have been removed to work with Feather Wiki specifically.
+ * 
  * @licence MIT
  */
 
-const defaultParagraphSeparatorString = 'defaultParagraphSeparator'
+// Shorten the names of these to help the minifier
 const fb = 'formatBlock'
-const addEventListener = (parent, type, listener) => parent.addEventListener(type, listener)
-const appendChild = (parent, child) => parent.appendChild(child)
-const createElement = tag => document.createElement(tag)
-const queryCommandState = command => document.queryCommandState(command)
-const queryCommandValue = command => document.queryCommandValue(command)
+const ael = (parent, type, listener) => parent.addEventListener(type, listener)
+const ac = (parent, child) => parent.appendChild(child)
+const ce = tag => document.createElement(tag)
+const qcs = command => document.queryCommandState(command)
 
 export const exec = (command, value = null) => document.execCommand(command, false, value)
 
-const defaultActions = {
-  bold: {
-    icon: '<b>B</b>',
-    title: 'Bold',
-    state: () => queryCommandState('bold'),
-    result: () => exec('bold')
-  },
-  italic: {
-    icon: '<i>I</i>',
-    title: 'Italic',
-    state: () => queryCommandState('italic'),
-    result: () => exec('italic')
-  },
-  underline: {
-    icon: '<u>U</u>',
-    title: 'Underline',
-    state: () => queryCommandState('underline'),
-    result: () => exec('underline')
-  },
-  strikethrough: {
-    icon: '<strike>S</strike>',
-    title: 'Strike-through',
-    state: () => queryCommandState('strikeThrough'),
-    result: () => exec('strikeThrough')
-  },
-  heading1: {
-    icon: '<b>H<sub>1</sub></b>',
-    title: 'Heading 1',
-    result: () => exec(fb, '<h1>')
-  },
-  heading2: {
-    icon: '<b>H<sub>2</sub></b>',
-    title: 'Heading 2',
-    result: () => exec(fb, '<h2>')
-  },
-  paragraph: {
-    icon: '&#182;',
-    title: 'Paragraph',
-    result: () => exec(fb, '<p>')
-  },
-  quote: {
-    icon: '&#8220; &#8221;',
-    title: 'Quote',
-    result: () => exec(fb, '<blockquote>')
-  },
-  olist: {
-    icon: '&#35;',
-    title: 'Ordered List',
-    result: () => exec('insertOrderedList')
-  },
-  ulist: {
-    icon: '&#8226;',
-    title: 'Unordered List',
-    result: () => exec('insertUnorderedList')
-  },
-  code: {
-    icon: '&lt;/&gt;',
-    title: 'Code',
-    result: () => exec(fb, '<pre>')
-  },
-  line: {
-    icon: '&#8213;',
-    title: 'Horizontal Line',
-    result: () => exec('insertHorizontalRule')
-  },
-  link: {
-    icon: '&#128279;',
-    title: 'Link',
-    result: () => {
-      const url = window.prompt('Enter the link URL')
-      if (url) exec('createLink', url)
-    }
-  },
-  image: {
-    icon: '&#128247;',
-    title: 'Image',
-    result: () => {
-      const url = window.prompt('Enter the image URL')
-      if (url) exec('insertImage', url)
-    }
-  }
-}
-
-const defaultClasses = {
-  actionbar: 'pell-actionbar',
-  button: 'pell-button',
-  content: 'pell-content',
-  selected: 'pell-button-selected'
-}
-
 export const init = settings => {
-  const actions = settings.actions
-    ? (
-      settings.actions.map(action => {
-        if (typeof action === 'string') return defaultActions[action]
-        else if (defaultActions[action.name]) return { ...defaultActions[action.name], ...action }
-        return action
-      })
-    )
-    : Object.keys(defaultActions).map(action => defaultActions[action])
+  const actions = [
+    { // bold
+      icon: '<b>B</b>',
+      title: 'Bold',
+      state: () => qcs('bold'),
+      result: () => exec('bold')
+    },
+    { // italic
+      icon: '<i>I</i>',
+      title: 'Italic',
+      state: () => qcs('italic'),
+      result: () => exec('italic')
+    },
+    { // underline
+      icon: '<u>U</u>',
+      title: 'Underline',
+      state: () => qcs('underline'),
+      result: () => exec('underline')
+    },
+    { // strikethrough
+      icon: '<strike>S</strike>',
+      title: 'Strike-through',
+      state: () => qcs('strikeThrough'),
+      result: () => exec('strikeThrough')
+    },
+    { // heading2
+      icon: '<b>H</b>',
+      title: 'Heading',
+      result: () => exec(fb, '<h2>'),
+    },
+    { // heading3
+      icon: '<b>H<sub>2</sub></b>',
+      title: 'Sub-Heading',
+      result: () => exec(fb, '<h3>'),
+    },
+    { // paragraph
+      icon: '&#182;',
+      title: 'Paragraph',
+      result: () => exec(fb, '<p>')
+    },
+    { // olist
+      icon: '&#35;',
+      title: 'Number List',
+      result: () => exec('insertOrderedList')
+    },
+    { // ulist
+      icon: '&#8226;',
+      title: 'Bullet List',
+      result: () => exec('insertUnorderedList')
+    },
+    { // quote
+      icon: '&#8220; &#8221;',
+      title: 'Quote',
+      result: () => exec(fb, '<blockquote>')
+    },
+    // { // code
+    //   icon: '&lt;/&gt;',
+    //   title: 'Code',
+    //   result: () => exec(fb, '<pre>')
+    // },
+    { // line
+      icon: '&#8213;',
+      title: 'Separator',
+      result: () => exec('insertHorizontalRule')
+    },
+    { // link
+      icon: '&#128279;',
+      title: 'Link',
+      result: () => {
+        const url = window.prompt('Enter the link URL')
+        if (url) exec('createLink', url)
+      }
+    },
+    { // externalImage
+      title: 'Link External Image',
+      icon: '🖼️',
+      result: () => {
+        const url = window.prompt('Enter the image URL');
+        if (url) exec('insertImage', url);
+      },
+    },
+    { // insertImage
+      title: 'Insert Image from File',
+      icon: '📸',
+      result: () => FW.img.upload(state, insert),
+    },
+    { // existingImage
+      title: 'Add Existing Image',
+      icon: '📎',
+      result: () => document.getElementById('g').showModal(),
+    },
+  ];
+  const classes = {
+    actionbar: 'pell-actionbar',
+    button: 'pell-button',
+    content: 'pell-content',
+    selected: 'pell-button-selected'
+  };
 
-  const classes = { ...defaultClasses, ...settings.classes }
-
-  const defaultParagraphSeparator = settings[defaultParagraphSeparatorString] || 'div'
-
-  const actionbar = createElement('div')
+  const actionbar = ce('div')
   actionbar.className = classes.actionbar
-  appendChild(settings.element, actionbar)
+  ac(settings.element, actionbar)
 
-  const content = settings.element.content = createElement('div')
+  const content = settings.element.content = ce('div')
   content.contentEditable = true
   content.className = classes.content
   content.oninput = ({ target: { firstChild } }) => {
-    if (firstChild && firstChild.nodeType === 3) exec(fb, `<${defaultParagraphSeparator}>`)
+    if (firstChild && firstChild.nodeType === 3) exec(fb, `<p>`)
     else if (content.innerHTML === '<br>') content.innerHTML = ''
     settings.onChange(content.innerHTML)
   }
   content.onkeydown = event => {
-    if (event.key === 'Enter' && queryCommandValue(fb) === 'blockquote') {
-      setTimeout(() => exec(fb, `<${defaultParagraphSeparator}>`), 0)
+    if (event.key === 'Enter' && document.queryCommandValue(fb) === 'blockquote') {
+      setTimeout(() => exec(fb, `<p>`), 0)
     }
   }
-  appendChild(settings.element, content)
+  ac(settings.element, content)
 
   actions.forEach(action => {
-    const button = createElement('button')
+    const button = ce('button')
     button.className = classes.button
     button.innerHTML = action.icon
     button.title = action.title
@@ -149,16 +145,16 @@ export const init = settings => {
 
     if (action.state) {
       const handler = () => button.classList[action.state() ? 'add' : 'remove'](classes.selected)
-      addEventListener(content, 'keyup', handler)
-      addEventListener(content, 'mouseup', handler)
-      addEventListener(button, 'click', handler)
+      ael(content, 'keyup', handler)
+      ael(content, 'mouseup', handler)
+      ael(button, 'click', handler)
     }
 
-    appendChild(actionbar, button)
+    ac(actionbar, button)
   })
 
   if (settings.styleWithCSS) exec('styleWithCSS')
-  exec(defaultParagraphSeparatorString, defaultParagraphSeparator)
+  exec('defaultParagraphSeparator', 'p')
 
   return settings.element
 }
