3695f238b78c827b9df4548838f6447b26831ba1
Robbie Antenesse <dev@alamantus.com>
2024-04-03 21:44:34 -0600

    Clear heading formatting with enter (#147) & add indent/outdent with tab in html editor; Use `html` to reduce code size

--- helpers/ed.js
+++ helpers/ed.js
@@ -133,43 +133,38 @@ export const init = settings => {
     },
   ];
 
-  const actionbar = ce('div')
-  actionbar.className = 'ed-actionbar'
-  actionbar.role = 'toolbar'
-  ac(settings.element, actionbar)
-
-  const content = settings.element.content = ce('div')
-  content.contentEditable = true
-  content.className = 'ed-content'
-  content.oninput = ({ target: { firstChild } }) => {
-    if (firstChild && firstChild.nodeType === 3) exec(fb, `<p>`)
-    else if (content.innerHTML === '<br>') content.innerHTML = ''
+  const content = settings.element.content = html`<div class="ed-content" contenteditable="true" oninput=${({ target: { firstChild } }) => {
+    if (firstChild && firstChild.nodeType === 3) exec(fb, '<p>');
+    else if (content.innerHTML === '<br>') content.innerHTML = '';
     settings.onChange(content.innerHTML)
-  }
-  content.onkeydown = event => {
-    if (event.key === 'Enter' && document.queryCommandValue(fb) === 'blockquote') {
-      setTimeout(() => exec(fb, `<p>`), 0)
+  }} onkeydown=${event => {
+    if (event.key === 'Enter' && document.queryCommandValue(fb).match(/blockquote|h\d/)) {
+      setTimeout(() => exec(fb, '<p>'), 0);
     }
-  }
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setTimeout(() => exec(event.shiftKey ? 'outdent' : 'indent'), 0);
+    }
+  }}></div>`;
+
+  const actionbar = html`<div class="ed-actionbar" role="toolbar">
+    ${actions.map(action => {
+      const button = html`<button class="ed-button" type="button" title="${action.title}" onclick=${() => action.result() && content.focus()}>
+        ${html.raw(action.icon)}
+      </button>`;
+
+      if (action.state) {
+        const handler = () => button.classList[action.state() ? 'add' : 'remove']('ed-selected')
+        ael(content, 'keyup', handler)
+        ael(content, 'mouseup', handler)
+        ael(button, 'click', handler)
+      }
+
+      return button
+    })}
+  </div>`;
+  ac(settings.element, actionbar)
   ac(settings.element, content)
-
-  actions.forEach(action => {
-    const button = ce('button')
-    button.className = 'ed-button'
-    button.innerHTML = action.icon
-    button.title = action.title
-    button.setAttribute('type', 'button')
-    button.onclick = () => action.result() && content.focus()
-
-    if (action.state) {
-      const handler = () => button.classList[action.state() ? 'add' : 'remove']('ed-selected')
-      ael(content, 'keyup', handler)
-      ael(content, 'mouseup', handler)
-      ael(button, 'click', handler)
-    }
-
-    ac(actionbar, button)
-  })
 
   exec('defaultParagraphSeparator', 'p')
 
